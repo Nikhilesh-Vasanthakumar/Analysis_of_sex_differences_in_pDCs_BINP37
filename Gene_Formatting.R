ca4ad6700550db7557346fdc10f71a3d167683d0
Nikhilesh-Vasanthakumar <v.nikhilesh23k@gmail.com>
2023-05-22 15:28:54 +0200

    Visulaizing the gene Expression and writing simplifying functions.

---
+++ Gene_Formatting.R
@@ -0,0 +1,15 @@
+#Script to replace the / with "" and seperate with ,
+
+#Eg NIK/HIL/ESH to "NIK","HIL","ESH"
+
+#Input: Gene list with / seperated genes
+#Output: Gene Vector
+
+#Read the gene list
+format_gene_list <- function(input_string) {
+  gene_list <- strsplit(input_string, "/")
+  gene_list <- unlist(gene_list)
+  return(gene_list)
+}
+
+
